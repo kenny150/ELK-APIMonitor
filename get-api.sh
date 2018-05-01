6164c233a97e010df4a4d8eae68faa17fe5611b7
Kennedy Alves <alvkennedy@gmail.com>
2018-05-01 19:21:08 -0300

    Removido comentario

--- get-api.sh
+++ get-api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
 FILE="/tmp/gitlab-rails/file-$(date +"%F-%H:%M").log"
-#curl -i --header "PRIVATE-TOKEN: odUrqqiAapVazLQyFG9u" "http://localhost:8081/api/v4/projects" -H "Accept:application/json" -o "$FILE"
+
 curl -XGET http://SEU-GITLAB:8081/api/v4/projects/1/repository/commits?private_token=SEU-TOKEN-PRIVADO \
 | jq '.' | tee "$FILE"
