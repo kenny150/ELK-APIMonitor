c0e041d76233b22577c0a35c1f5f4438e294c2e2
Kennedy Alves <alvkennedy@gmail.com>
2018-05-05 16:52:40 -0300

    Inseridos comentarios no script de coleta de dados, criado um novo logstash.conf

--- get-api.sh
+++ get-api.sh
@@ -1,6 +1,16 @@
 #!/bin/bash 
 
+
+##################################################
+#Autor Kennedy Alves                             #
+#Versão 1.0                                      #
+#Descrição Coleta de dados de qualquer API.      #
+##################################################
+
+
+#Local onde seus dados serão armazenaddos.
 FILE="/tmp/gitlab-rails/file-$(date +"%F-%H:%M").log"
 
+#Chamada para a API
 curl -XGET http://SEU-GITLAB:8081/api/v4/projects/1/repository/commits?private_token=SEU-TOKEN-PRIVADO \
 | jq '.' | tee "$FILE"
